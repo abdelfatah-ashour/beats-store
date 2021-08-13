2185375a32ab9a13f0d9bf19c0bd6a42fb175ef2
abdelfatah ashour <abdelfatahashour4@gmail.com>
2021-08-13 20:10:20 +0200

    UPDATE clean products screen

--- client/src/screens/Products.jsx
+++ client/src/screens/Products.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import HeadPage from "../components/HeadPage/HeadPage.jsx";
 import SpecificFilter from "../components/SpecificFilter/SpecificFilter.jsx";
 import {useDispatch, useSelector} from "react-redux";
