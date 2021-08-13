f3bb95d4f7b25450126e6323cb9aaedfb9bc6b2d
abdelfatah ashour <abdelfatahashour4@gmail.com>
2021-08-13 20:06:31 +0200

    UPDATE debugging in production fetch products

--- client/src/screens/Products.jsx
+++ client/src/screens/Products.jsx
@@ -14,10 +14,8 @@ import "../assets/css/Products.css";
 
 export default function Products() {
   const {allProducts, filter} = useSelector(state => state);
-  // eslint-disable-next-line no-unused-vars
-  const [currentPage, setCurrentPage] = useState(1);
   const dispatch = useDispatch();
-
+  console.log(allProducts, filter);
   useEffect(() => {
     dispatch(fetchAllProduct());
 
@@ -42,11 +40,7 @@ export default function Products() {
         <div className="layout-products">
           <div className="main-products">
             {filter.length === 0 &&
-              slice(
-                allProducts.products,
-                (currentPage - 1) * 12,
-                currentPage * 12
-              ).map((product, i) => {
+              slice(allProducts.products, 0, 12).map((product, i) => {
                 return (
                   <div key={i} className="one-product">
                     <div className="option">
@@ -89,52 +83,47 @@ export default function Products() {
               })}
 
             {filter.length > 0 &&
-              slice(filter, (currentPage - 1) * 12, currentPage * 12).map(
-                (product, i) => {
-                  return (
-                    <div key={i} className="one-product">
-                      <div className="option">
-                        <div>
-                          <AiOutlineHeart />
-                        </div>
-                        <div>
-                          <Link
-                            to={`/all-products/${product.category.toLowerCase()}/${
-                              product.name
-                            }/${product._id}`}
-                          >
-                            <BiSearch />
-                          </Link>
-                        </div>
+              slice(filter, 0, 12).map((product, i) => {
+                return (
+                  <div key={i} className="one-product">
+                    <div className="option">
+                      <div>
+                        <AiOutlineHeart />
                       </div>
-                      <div
-                        className="wrapper-img"
-                        style={{marginBottom: "1rem"}}
-                      >
-                        <img
-                          src={API + "/" + product.productImages[0]}
-                          alt={product.productImages[0]}
-                        />
-                        <div className="overlay"></div>
-                      </div>
-                      <div
-                        className="wrapper-content"
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                        }}
-                      >
-                        <h3 style={{marginBottom: "1rem"}}>{product.name}</h3>
-                        <h6 style={{marginBottom: "1rem", fontSize: "18px"}}>
-                          {product.price}
-                          <strong>$</strong>
-                        </h6>
+                      <div>
+                        <Link
+                          to={`/all-products/${product.category.toLowerCase()}/${
+                            product.name
+                          }/${product._id}`}
+                        >
+                          <BiSearch />
+                        </Link>
                       </div>
                     </div>
-                  );
-                }
-              )}
+                    <div className="wrapper-img" style={{marginBottom: "1rem"}}>
+                      <img
+                        src={API + "/" + product.productImages[0]}
+                        alt={product.productImages[0]}
+                      />
+                      <div className="overlay"></div>
+                    </div>
+                    <div
+                      className="wrapper-content"
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <h3 style={{marginBottom: "1rem"}}>{product.name}</h3>
+                      <h6 style={{marginBottom: "1rem", fontSize: "18px"}}>
+                        {product.price}
+                        <strong>$</strong>
+                      </h6>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
